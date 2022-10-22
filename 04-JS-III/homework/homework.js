81efa02502c89c430639e993174e815bf8078f90
Michelle734 <juliansimonetti96@gmail.com>
2022-10-22 15:44:50 -0300

    first commit

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -8,7 +8,7 @@ function devolverPrimerElemento(array) {
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
-  // Tu código:juli y cami
+  // Tu código:juli,camiymichelle
 }
 
 
